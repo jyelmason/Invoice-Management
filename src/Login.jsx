dd66b7a5c7c2736325303059abbba029ab0a4840
jyelmason <jyel.mason@gmail.com>
2026-08-12 11:10:45 -0400

    test

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,7 +3,7 @@ import { signInWithCustomToken } from 'firebase/auth'
 import { auth } from './firebase'
 
 
-const LOGIN_URL = 'https://us-central1-<YOUR_PROJECT_ID>.cloudfunctions.net/companyLogin'
+//const LOGIN_URL = 'https://us-central1-<YOUR_PROJECT_ID>.cloudfunctions.net/companyLogin'
 
 export default function Login ({onSuccess}) {
     const [username, setUsername] = useState('');
